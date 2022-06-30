fd538c18a55449695bc45c1ffa583bd5352a83e7
Mariana <marianaarguello2005@gmail.com>
2022-06-30 17:50:30 -0400

    hi

---
+++ mindMatters/videoView.swift
@@ -0,0 +1,8 @@
+//
+//  videoView.swift
+//  mindMatters
+//
+//  Created by Scholar on 6/30/22.
+//
+
+import Foundation
---
+++ youtubeVid.swift
@@ -0,0 +1,8 @@
+//
+//  youtubeVid.swift
+//  mindMatters
+//
+//  Created by Scholar on 6/30/22.
+//
+
+import Foundation
